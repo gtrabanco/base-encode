0d2a01c401fb127088874b00f4b516795defe21e
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2016-11-07 01:31:57 +0000

    Fixed definition file

--- Encode.class.d.ts
+++ Encode.class.d.ts
@@ -1,6 +1,5 @@
 declare class Encode {
-    constructor(private _alphabet:string);
-
+    constructor();
     public encode (num:number):string;
     public decode (str:string):number;
     public base():number;
