6623dc0996e6c1db799a4973430867822cc530f8
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2016-11-10 02:38:29 +0000

    Trying to solve problems with definition

--- Encode.class.d.ts
+++ Encode.class.d.ts
@@ -1,6 +1,13 @@
-declare class Encode {
-    constructor(alphabet:string);
-    public encode (num:number):string;
-    public decode (str:string):number;
-    public base():number;
+// Type definitions for base-encode
+// Project: https://github.com/gtrabanco/base-encode
+// Definitions by: Gabriel Trabanco Llano <https://gabi.soy>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+declare module Encode {
+
+    interface Encode {
+        constructor(alphabet: string);
+        encode (num: number): string;
+        decode (str: string): number;
+        base(): number;
+    }
 }
