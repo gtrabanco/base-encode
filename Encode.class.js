95b122f14c67678eff5bcddb2ea99057d636db82
Gabriel Trabanco Llano <gtrabanco@fwok.org>
2016-11-10 02:22:11 +0000

    Added module

--- Encode.class.js
+++ Encode.class.js
@@ -1,45 +1,47 @@
 "use strict";
-var Encode = (function () {
-    function Encode(_alphabet) {
-        if (_alphabet === void 0) { _alphabet = '0123456789abcdefhijkmnopqrsuvwxyzACBDEFHJKMNPQRSUVWXYZ'; }
-        this._alphabet = _alphabet;
-    }
-    Encode.prototype.encode = function (num) {
-        return this._encode(num);
-    };
-    Encode.prototype.decode = function (str) {
-        return this._decode(str);
-    };
-    Encode.prototype.base = function () {
-        return this._alphabet.length;
-    };
-    Encode.prototype._encode = function (num, encoded) {
-        if (encoded === void 0) { encoded = ''; }
-        var base = this.base();
-        if (num > 0) {
-            encoded += (this._alphabet[(num % base)]); //Remainder in division is the index in this._alphabet
-            num = Math.floor(num / base); // The next number to generate is the quotient Int part
-            return this._encode(num, encoded);
+var Encode;
+(function (Encode_1) {
+    var Encode = (function () {
+        function Encode(_alphabet) {
+            if (_alphabet === void 0) { _alphabet = '0123456789abcdefhijkmnopqrsuvwxyzACBDEFHJKMNPQRSUVWXYZ'; }
+            this._alphabet = _alphabet;
         }
-        else {
-            return encoded || '0';
-        }
-    };
-    Encode.prototype._decode = function (str) {
-        var base = this.base();
-        var result = 0;
-        if (str.length > 0) {
-            for (var x = 0; x < str.length; x++) {
-                //From right to left for the power so we need to get the power for
-                // the coded chars from left to right
-                var power = str.length - (1 + x); // equal to (str.length - 1 - x)
-                var charValue = this._alphabet.indexOf(str[power]);
-                result += charValue * (Math.pow(base, power));
+        Encode.prototype.encode = function (num) {
+            return this._encode(num);
+        };
+        Encode.prototype.decode = function (str) {
+            return this._decode(str);
+        };
+        Encode.prototype.base = function () {
+            return this._alphabet.length;
+        };
+        Encode.prototype._encode = function (num, encoded) {
+            if (encoded === void 0) { encoded = ''; }
+            var base = this.base();
+            if (num > 0) {
+                encoded += (this._alphabet[(num % base)]); //Remainder in division is the index in this._alphabet
+                num = Math.floor(num / base); // The next number to generate is the quotient Int part
+                return this._encode(num, encoded);
             }
-        }
-        return result;
-    };
-    return Encode;
-}());
-exports.Encode = Encode;
+            else {
+                return encoded || '0';
+            }
+        };
+        Encode.prototype._decode = function (str) {
+            var base = this.base();
+            var result = 0;
+            if (str.length > 0) {
+                for (var x = 0; x < str.length; x++) {
+                    //From right to left for the power so we need to get the power for
+                    // the coded chars from left to right
+                    var power = str.length - (1 + x); // equal to (str.length - 1 - x)
+                    var charValue = this._alphabet.indexOf(str[power]);
+                    result += charValue * (Math.pow(base, power));
+                }
+            }
+            return result;
+        };
+        return Encode;
+    }());
+})(Encode = exports.Encode || (exports.Encode = {}));
 //# sourceMappingURL=Encode.class.js.map
--- Encode.class.ts
+++ Encode.class.ts
@@ -1,61 +1,64 @@
 "use strict";
 
 
-export class Encode {
+export module Encode {
+    class Encode {
 
 
-    constructor(private _alphabet:string = '0123456789abcdefhijkmnopqrsuvwxyzACBDEFHJKMNPQRSUVWXYZ') { }
-
-
-    public encode (num:number):string  {
-
-        return this._encode(num);
-    }
-
-    public decode (str:string):number {
-        return this._decode(str);
-    }
-
-    public base():number {
-        return this._alphabet.length;
-    }
-
-    private _encode(num:number, encoded:string=''):string {
-
-        const base = this.base();
-
-        if (num > 0) {
-
-            encoded += (this._alphabet[(num % base)]); //Remainder in division is the index in this._alphabet
-            num = Math.floor(num / base); // The next number to generate is the quotient Int part
-
-            return this._encode(num, encoded);
-
-        } else {
-
-            return encoded || '0';
+        constructor(private _alphabet: string = '0123456789abcdefhijkmnopqrsuvwxyzACBDEFHJKMNPQRSUVWXYZ') {
         }
-    }
 
 
-    private _decode(str:string): number {
+        public encode(num: number): string {
 
-        const base = this.base();
+            return this._encode(num);
+        }
 
-        let result = 0;
+        public decode(str: string): number {
+            return this._decode(str);
+        }
 
-        if (str.length > 0) {
-            for (let x = 0; x < str.length; x++) {
-                //From right to left for the power so we need to get the power for
-                // the coded chars from left to right
-                let power = str.length - (1 + x); // equal to (str.length - 1 - x)
+        public base(): number {
+            return this._alphabet.length;
+        }
 
-                let charValue = this._alphabet.indexOf(str[power]);
+        private _encode(num: number, encoded: string = ''): string {
 
-                result += charValue * (Math.pow(base, power))
+            const base = this.base();
+
+            if (num > 0) {
+
+                encoded += (this._alphabet[(num % base)]); //Remainder in division is the index in this._alphabet
+                num = Math.floor(num / base); // The next number to generate is the quotient Int part
+
+                return this._encode(num, encoded);
+
+            } else {
+
+                return encoded || '0';
             }
         }
 
-        return result;
+
+        private _decode(str: string): number {
+
+            const base = this.base();
+
+            let result = 0;
+
+            if (str.length > 0) {
+                for (let x = 0; x < str.length; x++) {
+                    //From right to left for the power so we need to get the power for
+                    // the coded chars from left to right
+                    let power = str.length - (1 + x); // equal to (str.length - 1 - x)
+
+                    let charValue = this._alphabet.indexOf(str[power]);
+
+                    result += charValue * (Math.pow(base, power))
+                }
+            }
+
+            return result;
+        }
     }
 }
